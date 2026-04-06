59e282afd7ba707b6b4440ec071d314e141e813f
MizukiKusumoto <apple1167water@gmail.com>
2026-04-06 23:02:33 +0900

    微修正

--- frontistr/bar/job.sh
+++ frontistr/bar/job.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #PJM -g gb25
 #PJM -L rscgrp=short-o
-#PJM -L node=2
-#PJM --mpi proc=8
+#PJM -L node=4
+#PJM --mpi proc=16
 #PJM --omp thread=12
 #PJM -L elapse=01:00:00
 #PJM -j
@@ -19,7 +19,7 @@ module load hdf5/1.12.0
 
 cd ${PJM_O_WORKDIR}
 mpirun -n 1 hecmw_part1
-mpirun -n 8 fistr1
+mpirun -n 16 fistr1
 
 rm column_fistr_4.*
 rm column_fistr.res.*
