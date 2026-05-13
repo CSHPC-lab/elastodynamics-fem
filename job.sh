bb0d7f55f8cd70a6330d2a2847d013940498d66b
MizukiKusumoto <yoapple1167water@gmail.com>
2026-05-13 23:45:52 +0900

    numaアーキテクチャに合わせれば高速

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --partition=40g
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=48
+#SBATCH --cpus-per-task=12
 #SBATCH --time=120:00:00
 #SBATCH -o ./cpp_log/slurm.%j.out
 #SBATCH -e ./cpp_log/slurm.%j.err
@@ -11,7 +11,7 @@
 . /etc/profile.d/modules.sh
 module load nvhpc/25.1
 
-export OMP_NUM_THREADS=24        # 物理コア数に合わせる（SMT は使わない）
+export OMP_NUM_THREADS=6        # 物理コア数に合わせる（SMT は使わない）
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores          # "cores" 指定で各物理コアに 1 スレッドずつ
 export OMP_DYNAMIC=false
@@ -19,6 +19,6 @@ export OMP_WAIT_POLICY=active
 
 # export NV_ACC_TIME=1
 
-mpirun -n $SLURM_NTASKS ./a.out
+mpirun -n $SLURM_NTASKS --map-by numa --bind-to numa ./a.out
 
 echo "Job finished at $(date)"
